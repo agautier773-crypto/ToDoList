355df0df33bec3695b874a87815e0024a92049d2
Your Name <you@example.com>
2026-02-18 09:46:21 +0100

    Salut

---
+++ src/Controllers/CarteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+use App\Core\Controller;
+
+class CarteController extends Controller{
+
+    public function index()
+    {
+        var_dump("envoie tes codes");
+        }
+}
