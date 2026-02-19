0a6888804b1ebd845aca3d940fbd713f51277179
abc <abc@abc.abc>
2026-02-19 14:41:56 +0100

    tache: create update

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use App\Core\Session;
 use App\Core\Wizardvalidator;
 \App\Core\Session::getInstance();
 
-
+//var_dump(password_hash("Wood",PASSWORD_DEFAULT));
 $router = new App\Core\Router();
 
 $router -> addMiddleware([
@@ -18,7 +18,10 @@ $router
     ->post("/users/create", App\Controllers\UserController::class . "::store")
     ->get("/users/terms", App\Controllers\UserController::class . "::terms")
     ->get("/users/privacy", App\Controllers\UserController::class . "::privacy")
-
+    ->get("/tache/create", App\Controllers\TacheController::class . "::create")
+    ->post("/tache/create", App\Controllers\TacheController::class . "::store")
+    ->get("/tache/update/{id}", \App\Controllers\TacheController::class . "::edit")
+    ->post("/tache/update/{id}", \App\Controllers\TacheController::class . "::update")
 
 ->run();
 
