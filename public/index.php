cab5a01932f4a4a7b19e2e77ea598a30f8fa4a8c
GautierAlex <finesherbe420@gmail.com>
2026-02-20 09:07:56 +0100

    Ajout middleware auth + fix redirect apres connexion

--- public/index.php
+++ public/index.php
@@ -9,23 +9,25 @@ $router = new App\Core\Router();
 
 
 $router -> addMiddleware([
+    "auth" => App\Core\Middlewares\AuthMiddlewares::class,
+
     // ajout des middlewares
 ]);
 $router
    // ajout des routes
     ->get("/", App\Controllers\HomepageController::class ."::home")
-    ->get("/users/create", App\Controllers\UserController::class . "::create")
-    ->post("/users/create", App\Controllers\UserController::class . "::store")
+    ->get("/users/create", App\Controllers\UserController::class . "::create")-> middleware("auth")
+    ->post("/users/create", App\Controllers\UserController::class . "::store")-> middleware("auth")
     ->get("/users/terms", App\Controllers\UserController::class . "::terms")
     ->get("/users/privacy", App\Controllers\UserController::class . "::privacy")
-    ->get("/tache/create", App\Controllers\TacheController::class . "::create")
-    ->post("/tache/create", App\Controllers\TacheController::class . "::store")
-    ->get("/tache/update/{id}", \App\Controllers\TacheController::class . "::edit")
-    ->post("/tache/update/{id}", \App\Controllers\TacheController::class . "::update")
 
-    ->get("/tache", App\Controllers\TacheController::class ."::index")
-    ->get("/tache/show/{id}", App\Controllers\TacheController::class ."::show")
-    ->get("/tache/delete/{id}", App\Controllers\TacheController::class ."::delete")
+    ->get("/tache/create", App\Controllers\TacheController::class . "::create")-> middleware("auth")
+    ->post("/tache/create", App\Controllers\TacheController::class . "::store")-> middleware("auth")
+    ->get("/tache/update/{id}", \App\Controllers\TacheController::class . "::edit")-> middleware("auth")
+    ->post("/tache/update/{id}", \App\Controllers\TacheController::class . "::update")-> middleware("auth")
+    ->get("/tache", App\Controllers\TacheController::class ."::index")-> middleware("auth")
+    ->get("/tache/show/{id}", App\Controllers\TacheController::class ."::show")-> middleware("auth")
+    ->get("/tache/delete/{id}", App\Controllers\TacheController::class ."::delete")-> middleware("auth")
 
     ->get("/login", App\Controllers\AuthController::class . "::login")
     ->post("/login", App\Controllers\AuthController::class . "::Attemptlogin")
